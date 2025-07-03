97d80290659bda09526f38b8acd0ffc31622cc67
simpleskans <simpleskans@gmail.com>
2025-07-03 11:41:08 -0300

    Visualização de imagens em tela cheia

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.createElement("div");
+    modal.id = "img-modal";
+    modal.className = "closeable";
+    modal.innerHTML = "<img src=''>";
+    document.body.appendChild(modal);
+
+    const modalImg = modal.querySelector("img");
+
+    document.querySelectorAll(".zoom-img").forEach(img => {
+        img.style.cursor = "zoom-in";
+        img.addEventListener("click", () => {
+            modal.style.display = "flex";
+            modalImg.src = img.src;
+        });
+    });
+
+    modal.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+});
